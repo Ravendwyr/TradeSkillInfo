f63c7b7b3ef287dcd92f2f32293bff4fb760b53e
Ethan Centaurai <ethancentaurai@outlook.com>
2012-11-26 10:17:21 +0000

    Fix a copy/paste fail and improve the LDB tooltip code.

--- DataBroker.lua
+++ DataBroker.lua
@@ -1,4 +1,5 @@
 
+local L = LibStub("AceLocale-3.0"):GetLocale("TradeskillInfo")
 local Broker = LibStub("LibDataBroker-1.1"):NewDataObject("TradeSkillInfo")
 
 Broker.type = "launcher"
@@ -14,7 +15,7 @@ Broker.OnClick = function(frame, button)
 end
 
 Broker.OnTooltipShow = function(tooltip)
-	tooltip:SetText("TradeSkillInfo")
-	tooltip:AddLine("|cff30e030" .. L["Left Click"] .. "|r: " .. L["Open main window"])
-	tooltip:AddLine("|cff30e030" .. L["Right Click"] .. "|r: " .. L["Open configuration window"])
+	tooltip:AddLine("TradeSkill Info")
+	tooltip:AddLine(L["Left Click"] .. ": " .. L["Open main window"] .. ".", 0, 1, 0)
+	tooltip:AddLine(L["Right Click"] .. ": " .. L["Open configuration window"] .. ".", 0, 1, 0)
 end
