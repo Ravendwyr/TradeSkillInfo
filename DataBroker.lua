29d91898f7b678f36823e75c5e51d3637732ab2b
Ethan Centaurai <ethancentaurai@outlook.com>
2012-11-27 11:51:06 +0000

    Prevent a potential for no-lib users.

--- DataBroker.lua
+++ DataBroker.lua
@@ -1,21 +1,25 @@
 
 local L = LibStub("AceLocale-3.0"):GetLocale("TradeskillInfo")
-local Broker = LibStub("LibDataBroker-1.1"):NewDataObject("TradeSkillInfo")
+local LDB = LibStub("LibDataBroker-1.1", true)
 
-Broker.type = "launcher"
-Broker.text = "TradeSkillInfo"
-Broker.icon = "Interface\\Icons\\INV_Elemental_Mote_Nether"
+if LDB then
+	local Broker = LDB:NewDataObject("TradeSkillInfo")
 
-Broker.OnClick = function(frame, button)
-	if button == "LeftButton" then
-		TradeskillInfo:UI_Toggle()
-	elseif button == "RightButton" then
-		TradeskillInfo:ConfigToggle()
+	Broker.type = "launcher"
+	Broker.text = "TradeSkill Info"
+	Broker.icon = "Interface\\Icons\\INV_Elemental_Mote_Nether"
+
+	Broker.OnClick = function(frame, button)
+		if button == "LeftButton" then
+			TradeskillInfo:UI_Toggle()
+		elseif button == "RightButton" then
+			TradeskillInfo:ConfigToggle()
+		end
 	end
-end
 
-Broker.OnTooltipShow = function(tooltip)
-	tooltip:AddLine("TradeSkill Info")
-	tooltip:AddLine(L["Left Click"] .. ": " .. L["Open main window"] .. ".", 0, 1, 0)
-	tooltip:AddLine(L["Right Click"] .. ": " .. L["Open configuration window"] .. ".", 0, 1, 0)
+	Broker.OnTooltipShow = function(tooltip)
+		tooltip:AddLine("TradeSkill Info")
+		tooltip:AddLine(L["Left Click"] .. ": " .. L["Open main window"] .. ".", 0, 1, 0)
+		tooltip:AddLine(L["Right Click"] .. ": " .. L["Open configuration window"] .. ".", 0, 1, 0)
+	end
 end
