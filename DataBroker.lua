5e77343706bc9a0b0005faef2b9b82a16b003125
Ethan Centaurai <ethancentaurai@outlook.com>
2013-03-06 09:38:50 +0000

    Tweak the broker object slightly.

--- DataBroker.lua
+++ DataBroker.lua
@@ -3,13 +3,13 @@ local L = LibStub("AceLocale-3.0"):GetLocale("TradeskillInfo")
 local LDB = LibStub("LibDataBroker-1.1", true)
 
 if LDB then
-	local Broker = LDB:NewDataObject("TradeSkillInfo")
+	local object = LDB:NewDataObject("TradeSkillInfo")
 
-	Broker.type = "launcher"
-	Broker.text = "TradeSkill Info"
-	Broker.icon = "Interface\\Icons\\INV_Elemental_SpiritOfHarmony_2"
+	object.type = "launcher"
+	object.text = "TradeSkillInfo"
+	object.icon = "Interface\\Icons\\INV_Elemental_SpiritOfHarmony_2"
 
-	Broker.OnClick = function(frame, button)
+	object.OnClick = function(_, button)
 		if button == "LeftButton" then
 			TradeskillInfo:UI_Toggle()
 		elseif button == "RightButton" then
@@ -17,8 +17,8 @@ if LDB then
 		end
 	end
 
-	Broker.OnTooltipShow = function(tooltip)
-		tooltip:AddLine("TradeSkill Info")
+	object.OnTooltipShow = function(tooltip)
+		tooltip:AddLine("TradeSkillInfo")
 		tooltip:AddLine(L["Left Click"] .. ": " .. L["Open main window"] .. ".", 0, 1, 0)
 		tooltip:AddLine(L["Right Click"] .. ": " .. L["Open configuration window"] .. ".", 0, 1, 0)
 	end
